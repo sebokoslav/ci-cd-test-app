4f6e33c9ce07a7bd29c7c022ea36830e869bd080
Jaroslav Šebok <jaro.sebok@gmail.com>
2023-04-23 14:26:03 +0200

    update-version.ps1 filename param

--- config/update-version.ps1
+++ config/update-version.ps1
@@ -3,8 +3,10 @@ param (
     [Parameter(Position=1)]
     $OldImageName,
     [Parameter(Position=2)]
-    $NewImageName
+    $NewImageName,
+    [Parameter(Position=3)]
+    $FileName
 )
-$content = Get-Content -Path 'environment.yaml'
+$content = Get-Content -Path $FileName
 $newContent = $content -replace $OldImageName, $NewImageName
-$newContent | Set-Content -Path 'environment.yaml'
+$newContent | Set-Content -Path $FileName
